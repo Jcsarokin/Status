129eced230c1ff9757fbc549cbc48e34d5e8cfba
Julian Sarokin <Julian@sellsimple.com>
2014-02-17 01:09:30 -0800

    bunch of stuff sucka

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Status::Application.config.secret_key_base = 'bb27d997559b497e168a02776fbfa257e2167dbd23948b0082ffb279cd8ea0eafc1fd6b34acb05c70fc7276cdd669e95b2956b026055b46e877d766292cd0323'
